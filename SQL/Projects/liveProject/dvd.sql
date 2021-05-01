2537f12258d21d321d19abc30d3d3f661b788ed6
MsDiala <diala-is-diala@hotmail.com>
2021-05-01 18:44:00 +0300

    dn

--- SQL/Projects/liveProject/dvd.sql
+++ SQL/Projects/liveProject/dvd.sql
@@ -1,5 +1,5 @@
 
--- Q1 :
+-- Q1 : // we are going to use inner join to 4 tables
 select first_name as manager_firstname ,last_name as manager_last_name ,  address , district , city ,country from public.store
 inner join public.staff
 on public.store.manager_staff_id = public.staff.store_id
@@ -10,18 +10,18 @@ on public.city.city_id = public.address.city_id
 inner join public.country
 on public.country.country_id = public.city.country_id ; 
 
--- Q2 :
+-- Q2 : 
 select store_id , inventory_id , title , rating , rental_rate , replacement_cost  from public.film
 inner join public.inventory
 on public.inventory.film_id = public.film.film_id;
 
--- Q3 :
+-- Q3 :// Avg inv
 select store_id ,rating, count(inventory_id) as inventory_items from public.film
 inner join public.inventory
 on public.inventory.film_id = public.film.film_id
 group by  rating , store_id  
 order by rating asc ;
--- Q4 :
+-- Q4 : //???
 
 -- Q5 :
 select customer.first_name, customer.last_name, customer.store_id, customer.activebool, address.address,
